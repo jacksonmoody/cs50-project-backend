495290a7f056941ba81b83f8b97abdda2d144a21
Jackson <jackson@jacksonmoody.com>
2022-11-26 12:41:12 -0700

    Added JSON

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', method = ['GET'])
+
 def api():
-    return "Hello, World!"
+    return jsonify({"message": "Hello World"})
