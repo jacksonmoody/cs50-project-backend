5c7358e1a5fb513d7712e1129320bf2c96ede7e7
Jackson <jackson@jacksonmoody.com>
2022-11-26 12:53:45 -0700

    Changed route

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request, send_file
 
 app = Flask(__name__)
 
-@app.route("/", method=["GET"])
+@app.route("/")
 def api():
-    return "Hello, World!"
+    return jsonify({
+        "message": "Hello World",
+        })
