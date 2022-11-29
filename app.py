bc9396267f739c99042977a5ceaa15fb0c2a0836
Jackson <jackson@jacksonmoody.com>
2022-11-29 15:26:21 -0500

    Added credentials

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def youtubeapi():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secret2.json', SCOPES)
+                'google-credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
 
         with open('token.json', 'w') as token:
