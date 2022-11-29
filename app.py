639ab51cd000e886219a2642f6bb79685cfcc8b0
Jackson <jackson@jacksonmoody.com>
2022-11-28 23:20:23 -0500

    Added the scheduler back

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, jsonify, request, send_file
+from apscheduler.schedulers.blocking import BlockingScheduler
 import requests
 import os.path
 
 app = Flask(__name__)
 
+sched = BlockingScheduler()
+
 categories = {'sports', 'politics', 'business', 'entertainment', 'technology', 'science', 'health'}
+
+nyt_result = {}
 
 def nytapi():
     response = requests.get("https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=FgKjzYiiamFAfUJMbpPnqkn7u3ManknD")
@@ -12,9 +17,11 @@ def nytapi():
     nyt_result = response
     return nyt_result
 
-nyt_result = {}
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    nyt_result = nytapi()
 
-nyt_result = nytapi()
+sched.start()
 
 @app.route("/")
 def api():
