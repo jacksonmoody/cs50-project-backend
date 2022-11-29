50177c07380cb976f4e5cc66385afcfeb6162d40
Jackson <jackson@jacksonmoody.com>
2022-11-28 23:15:01 -0500

    Troubleshooting more :(

--- app.py
+++ app.py
@@ -2,55 +2,11 @@ from flask import Flask, jsonify, request, send_file
 import requests
 import os.path
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
-
 app = Flask(__name__)
 
 categories = {'sports', 'politics', 'business', 'entertainment', 'technology', 'science', 'health'}
 
-youtube_result = {}
 nyt_result = {}
-
-def youtubeapi():
-    creds = None
-
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secret2.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        service = build('youtube', 'v3', credentials=creds)
-        request = service.search().list(
-            part="snippet",
-            maxResults=1,
-            q="Jiminy Cricket",
-            regionCode="US"
-        )
-        response = request.execute()
-        title = response['items'][0]['snippet']['title']
-        description = response['items'][0]['snippet']['description']
-        thumbnail = response['items'][0]['snippet']['thumbnails']['high']['url']
-        youtube_result = {'title': title, 'description': description, 'thumbnail': thumbnail}
-        return youtube_result
-
-    except:
-        print("Failed")
 
 def nytapi():
     response = requests.get("https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=FgKjzYiiamFAfUJMbpPnqkn7u3ManknD")
@@ -61,6 +17,5 @@ def nytapi():
 @app.route("/")
 def api():
     return jsonify({
-        "youtube_api": youtube_result,
         "nyt_api": nyt_result
         })
