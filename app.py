c0177c279d1d8d98ddf9f762488e15132975ffa1
Jackson <jackson@jacksonmoody.com>
2022-12-01 20:21:42 -0500

    Import Google Auth

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ import requests
 import os.path
 import time
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 class Config:
     SCHEDULER_API_ENABLED = True
 
