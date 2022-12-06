0e93788a86e7fa76aad189539e10629310cf073d
Jackson <jackson@jacksonmoody.com>
2022-12-06 01:53:43 -0500

    Removed category

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def init():
     scheduler = APScheduler()
     scheduler.init_app(app)
     scheduler.start()
-    scheduler.add_job(id='mainapi', func=mainapi, trigger='interval', minutes=5)
+    scheduler.add_job(id='mainapi', func=mainapi, trigger='interval', minutes=2.5)
 
     endpoint = "https://www.googleapis.com/oauth2/v4/token"
     
@@ -46,7 +46,7 @@ def init():
    
 SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
 
-master_list = ['sports', 'art', 'technology', 'business', 'entertainment', 'science', 'politics']
+master_list = ['sports', 'technology', 'business', 'entertainment', 'science', 'politics']
 
 nyt_result = {}
 youtube_result = {}
@@ -55,14 +55,13 @@ wiki_result = {}
 def nytapi(term):
     
     sports = ["Sports"]
-    art = ["Arts", "Books", "Style"]
     technology = ["Automobiles", "Technology"]
     business = ["Business Day", "Business"]
-    entertainment = ["Culture", "Dining", "Food", "Magazine", "Movies", "T Magazine", "Technology", "The Upshot","Travel"]
+    entertainment = ["Arts", "Books", "Style", "Culture", "Dining", "Food", "Magazine", "Movies", "T Magazine", "Technology", "The Upshot","Travel"]
     science = ["Science","Upshot"]
     politics = ["Metro", "Metropolitan", "National", "Politics", "U.S.", "Washington", "World"]
     
-    nyt_dict = {'sports': sports, 'art': art, 'technology': technology, 'business': business, 'entertainment': entertainment, 'science': science, 'politics': politics}
+    nyt_dict = {'sports': sports, 'technology': technology, 'business': business, 'entertainment': entertainment, 'science': science, 'politics': politics}
 
     global nyt_result
     articles = {}
@@ -109,13 +108,12 @@ def youtubeapi(term):
     sports = ["/m/06ntj", "/m/0jm_", "/m/018jz", "/m/018w8", "/m/01cgz", "/m/09xp_", "/m/02vx4", "/m/037hz", "/m/03tmr", "/m/01h7lh", "/m/0410tth", "/m/07bs0", "m/07_53"]
     politics = ["/m/05qt0", "/m/01h6rj", "/m/06bvp"]
     business = ["/m/09s1f"]
-    entertainment = ["/m/02jjt", "/m/09kqc", "/m/02vxn", "/m/066wd", "/m/0f2f9", "/m/07bxq", "/m/03glg", "/m/068hy"]
+    entertainment = ["/m/02jjt", "/m/09kqc", "/m/02vxn", "/m/066wd", "/m/0f2f9", "/m/07bxq", "/m/03glg", "/m/068hy", "/m/032tl", "/m/04rlf", "/m/05qjc", "/m/041xxh"]
     technology = ["/m/07c1v", "/m/07yv9"]
     science = ["/m/01k8wb"]
-    art = ["/m/032tl", "/m/04rlf", "/m/05qjc", "/m/041xxh"]
     videos = {}
     videos[term] = []
-    youtube_dict = {"sports": sports, "politics": politics, "business": business, "entertainment": entertainment, "technology": technology, "science": science, "art": art}
+    youtube_dict = {"sports": sports, "politics": politics, "business": business, "entertainment": entertainment, "technology": technology, "science": science}
 
     category = random.choice(youtube_dict[term])
 
@@ -148,11 +146,10 @@ def wikiapi(term):
     sports = ["Sports", "Recreation", "Air sports", "American football", "Auto racing", "Baseball terminology", "Basketball", "Horse racing", "Ice hockey", "Olympic Games", "Whitewater sports"]
     politics = ["Lists of politicians", "Politics", "Political activism", "Clothing in politics", "Political communication", "Comparative politics", "Cultural politics", "Election campaigning", "Political philosophy", "Political theories"]
     business = ["Chief executive officers", "Billionaires", "Real estate", "Finance", "Business", "Paradoxes in economics", "Money", "Industries (economics)", "Financial markets", "Investment", "Business economics", "Business ethics", "Business economics", "Business terms", "Sports business"]
-    entertainment = ["Entertainment", "Lists of games", "Toys", "Film", "Internet", "Television", "Mass media franchises", "Humour", "Entertainment occupations", "Amusement parks", "Gaming", "Film characters", "History of film", "Cinemas and movie theaters", "Celebrity reality television series", "Comedy", "Unofficial observances", "Satire"]
+    entertainment = ["Entertainment", "Lists of games", "Toys", "Film", "Internet", "Television", "Mass media franchises", "Humour", "Entertainment occupations", "Amusement parks", "Gaming", "Film characters", "History of film", "Cinemas and movie theaters", "Celebrity reality television series", "Comedy", "Unofficial observances", "Satire", "Classical studies", "Critical theory", "Culture", "Humanities", "Folklore", "Performing arts", "Visual arts", "Economics of the arts and literature", "Arts occupations", "Fiction", "Fiction anthologies", "Clowning", "Storytelling", "Variety shows", "Theatre"]
     technology = ["Explorers", "Sports inventors and innovators", "Inventors", "Artificial intelligence", "Computer architecture", "Embedded systems", "Semiconductors", "Telecommunications", "Civil engineering", "Aerospace engineering", "History of the automobile", "Cycling", "Public transport", "Road transport"]
     science = ["Climate change", "Nature conservation", "Pollution", "Biology", "Zoology", "Neuroscience", "Humans", "Plants", "Space", "Astronomy", "Chemistry", "Climate", "Physics-related lists", "Space", "Energy", "Lists of things named after scientists"]
-    art = ["Classical studies", "Critical theory", "Culture", "Humanities", "Folklore", "Performing arts", "Visual arts", "Economics of the arts and literature", "Arts occupations", "Fiction", "Fiction anthologies", "Clowning", "Storytelling", "Variety shows", "Theatre"]
-    wiki_dict = {"sports": sports, "politics": politics, "business": business, "entertainment": entertainment, "technology": technology, "science": science, "art": art}
+    wiki_dict = {"sports": sports, "politics": politics, "business": business, "entertainment": entertainment, "technology": technology, "science": science}
 
     category = random.choice(wiki_dict[term])
     url = "https://en.wikipedia.org/w/api.php"
